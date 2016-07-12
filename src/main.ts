7aa2f6a8f7b57fc3947206324769d7d23f104e90
Trotyl <trotyl@qq.com>
2016-07-12 22:10:25 +0800

    Fulfill basic functionality.

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,8 @@
+import * as angular from 'angular'
+
 const env = process.env.NODE_ENV || 'local'
 console.log(`App is starting in env: ${env}`)
+
+export const app = 'app'
+
+angular.module(app, [])
