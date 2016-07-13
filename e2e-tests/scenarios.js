e6121fc04ad8fab8e0e4edfe42fedead2d892e7e
Trotyl <trotyl@qq.com>
2016-07-13 21:35:53 +0800

    Finish step-5: Filtering Repeaters.

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -3,10 +3,23 @@
 // Angular E2E Testing Guide:
 // https://docs.angularjs.org/guide/e2e-testing
 
-describe('My app', function () {
+describe('My app', () => {
 
-    beforeEach(function () {
+    beforeEach(() => {
         browser.get('index.html')
     })
 
+    it('should filter the phone list as a user types into the search box', () => {
+        const phoneList = element.all(by.repeater('phone in phoneList.phones'))
+        const query = element(by.model('phoneList.query'))
+
+        expect(phoneList.count()).toBe(3)
+
+        query.sendKeys('nexus')
+        expect(phoneList.count()).toBe(1)
+
+        query.clear()
+        query.sendKeys('motorola')
+        expect(phoneList.count()).toBe(2)
+    })
 })
